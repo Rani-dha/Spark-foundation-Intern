46468019e1a18bbb87797e134c4443f4b88d038e
Dharani T <gct.it18.dharani@gmail.com>
2021-01-22 14:02:34 +0530

    index page

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Web development and Design Intern</title>
+    <style>
+        a {
+            margin: 5x;
+        }
+
+        td {
+            width: 30%;
+            height: 30%;
+        }
+    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body class="lead">
+    <nav class="navbar navbar-dark navbar-expand-sm fixed-top bg-info">
+
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Navbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand mr-auto" href="index.php"></a>
+
+            <div class="collapse navbar-collapse" id="Navbar">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active"><a class="nav-link" href="#"><span style="margin-right:5px;" class="fa fa-home"></span>Home</a></li>
+                    <li class="nav-item text-white"><a class="nav-link" href="customer.php">Customers</a></li>
+                    <li class="nav-item text-white"><a class="nav-link" href="transfer.php">Transfer Money</a></li>
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+    </div>
+    </div>
+    <div class="row row-content align-items-center">
+
+        <div class="col col-sm order-sm-first col-md">
+            <div class="media">
+                <img class="d-flex mr-3 img-thumbnail align-self-center" src="savingsjpg.jpg" alt="family img" style="margin:76px 50px 10px 50px;">
+                <div class="media-body" style="margin:96px 50px 10px 50px;">
+                    <h3 class="text-muted"><em>The Spark Foundation Bank</em></h3>
+                    <p class="text-muted">The Spark Foundation is an Indian multinational, public sector banking and financial services company. It is the third largest public sector bank in India, with 131 million customers, a total business of US$218 billion, and a global presence of 100 overseas offices</p>
+                    <a class="btn btn-outline-info btn-small" href="customer.php" role="button" style="margin:5px;">Customers</a>
+                    <a class="btn btn-outline-info btn-small" href="transfer.php" role="button" style="margin:5px;">Transfer money</a><br /><br />
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
